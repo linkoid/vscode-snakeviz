f1a245f8af4846ad1bec71f70d4b265bef1f3efa
linkoid <36754150+linkoid@users.noreply.github.com>
2024-07-22 18:30:46 -0400

    Use python extension API to get interpreter.
    Python extension API is also used to get environment variables.
    Fixes: #1

--- src/snakevizEditor.ts
+++ src/snakevizEditor.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import * as child_process from 'child_process';
+import { EnvironmentVariables, PythonExtension, Resource } from '@vscode/python-extension';
 
 
 
@@ -16,7 +17,7 @@ class SnakevizDocument implements vscode.CustomDocument {
         this.snakevizUrl = new Promise<string>((resolve, reject) => {
             resolveSnakeVizUrl = resolve;
 		});
-        child.stdout?.on("data", (chunk) => {
+        child.stdout!.on("data", (chunk) => {
             chunk = chunk.toString();
             let line: string;
             for (line of chunk.splitLines()) {
@@ -25,13 +26,17 @@ class SnakevizDocument implements vscode.CustomDocument {
                 }
             }
         });
+        child.stderr!.on("data", (chunk) => {
+            vscode.window.showErrorMessage(chunk.toString());
+        });
     }
 
     public static async create(uri: vscode.Uri): Promise<SnakevizDocument> {
         const child = child_process.spawn(
-            await SnakevizDocument.getPythonInterpreter(),
-            ["-m", "snakeviz", "--server", uri.path], {
+            await SnakevizDocument.getPythonInterpreter(uri),
+            ["-m", "snakeviz", "--server", uri.fsPath], {
                 env: {
+                    ... await SnakevizDocument.getPythonEnviornmentVariables(uri),
                     // eslint-disable-next-line @typescript-eslint/naming-convention
                     "PYTHONUNBUFFERED": "1",
                 }
@@ -41,15 +46,36 @@ class SnakevizDocument implements vscode.CustomDocument {
         return new SnakevizDocument(uri, child);
     }
 
-    static async getPythonInterpreter() {
-        const extension = vscode.extensions.getExtension("ms-python.python");
-        if (!extension) {
-            return "python";
+    static async getPythonInterpreter(resource?: Resource) {
+        let executable: string | undefined;
+
+        const pythonApi: PythonExtension = await PythonExtension.api();
+        const environmentPath = pythonApi.environments.getActiveEnvironmentPath(resource);
+        const environment = await pythonApi.environments.resolveEnvironment(environmentPath);
+        if (environment) {
+            executable ??= environment.executable.uri?.fsPath;
         }
-        if (!extension.isActive) {
-            await extension.activate();
+
+        // Fallback to using system-wide "python" as the interpreter.
+        executable ??= "python";
+
+        return executable;
+    }
+
+    static async getPythonEnviornmentVariables(resource?: Resource) {
+        let env: NodeJS.ProcessEnv | EnvironmentVariables | undefined;
+
+        const pythonApi: PythonExtension = await PythonExtension.api();
+        const environmentPath = pythonApi.environments.getActiveEnvironmentPath(resource);
+        const environment = await pythonApi.environments.resolveEnvironment(environmentPath);
+        if (environment) {
+            env ??= pythonApi.environments.getEnvironmentVariables(resource);
         }
-        return extension.exports.settings.getExecutionDetails().execCommand[0];
+        
+        // Fallback to returning the current enviornment
+        env ??= process.env;
+
+        return env;
     }
 
     public get uri(): vscode.Uri { return this._uri; }
